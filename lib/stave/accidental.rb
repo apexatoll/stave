db8c24c67d49ea726fd04b3e33c327deeeedf92d
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-06-29 22:58:06 +0100

    Implement Accidental#offset

--- lib/stave/accidental.rb
+++ lib/stave/accidental.rb
@@ -1,9 +1,9 @@
 module Stave
   class Accidental < Core::Lookup
-    variant :double_flat
-    variant :flat
-    variant :natural
-    variant :sharp
-    variant :double_sharp
+    variant :double_flat, offset: -2
+    variant :flat, offset: -1
+    variant :natural, offset: 0
+    variant :sharp, offset: 1
+    variant :double_sharp, offset: 2
   end
 end
