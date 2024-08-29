58bf08d2a72c87345e32dd7fe548bec286becb2a
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-08-29 23:58:12 +0100

    Create internal Interval::Number lookup

--- lib/stave/theory/interval.rb
+++ lib/stave/theory/interval.rb
@@ -1,6 +1,25 @@
 module Stave
   module Theory
     class Interval < Core::Lookup
+      class Number < Core::Lookup
+        variant :one, to_i: 1, size: 0
+        variant :two, to_i: 2, size: 2
+        variant :three, to_i: 3, size: 4
+        variant :four, to_i: 4, size: 5
+        variant :five, to_i: 5, size: 7
+        variant :six, to_i: 6, size: 9
+        variant :seven, to_i: 7, size: 11
+        variant :eight, to_i: 8, size: 12
+        variant :nine, to_i: 9, size: 14
+        variant :eleven, to_i: 11, size: 17
+        variant :thirteen, to_i: 13, size: 21
+
+        def degree
+          to_i >= 8 ? to_i - 7 : to_i
+        end
+
+        def to_s = to_i.to_s
+      end
     end
   end
 end
--- spec/stave/theory/interval_spec.rb
+++ spec/stave/theory/interval_spec.rb
@@ -1,3 +1,113 @@
 RSpec.describe Stave::Theory::Interval do
-  pending :implementation
+  describe Stave::Theory::Interval::Number do
+    describe ":one" do
+      subject(:number) { described_class.one }
+
+      it "sets the expected attributes" do
+        expect(number).to have_attributes(
+          degree: 1, size: 0, to_i: 1, to_s: "1"
+        )
+      end
+    end
+
+    describe ":two" do
+      subject(:number) { described_class.two }
+
+      it "sets the expected attributes" do
+        expect(number).to have_attributes(
+          degree: 2, size: 2, to_i: 2, to_s: "2"
+        )
+      end
+    end
+
+    describe ":three" do
+      subject(:number) { described_class.three }
+
+      it "sets the expected attributes" do
+        expect(number).to have_attributes(
+          degree: 3, size: 4, to_i: 3, to_s: "3"
+        )
+      end
+    end
+
+    describe ":four" do
+      subject(:number) { described_class.four }
+
+      it "sets the expected attributes" do
+        expect(number).to have_attributes(
+          degree: 4, size: 5, to_i: 4, to_s: "4"
+        )
+      end
+    end
+
+    describe ":five" do
+      subject(:number) { described_class.five }
+
+      it "sets the expected attributes" do
+        expect(number).to have_attributes(
+          degree: 5, size: 7, to_i: 5, to_s: "5"
+        )
+      end
+    end
+
+    describe ":six" do
+      subject(:number) { described_class.six }
+
+      it "sets the expected attributes" do
+        expect(number).to have_attributes(
+          degree: 6, size: 9, to_i: 6, to_s: "6"
+        )
+      end
+    end
+
+    describe ":seven" do
+      subject(:number) { described_class.seven }
+
+      it "sets the expected attributes" do
+        expect(number).to have_attributes(
+          degree: 7, size: 11, to_i: 7, to_s: "7"
+        )
+      end
+    end
+
+    describe ":eight" do
+      subject(:number) { described_class.eight }
+
+      it "sets the expected attributes" do
+        expect(number).to have_attributes(
+          degree: 1, size: 12, to_i: 8, to_s: "8"
+        )
+      end
+    end
+
+    describe ":nine" do
+      subject(:number) { described_class.nine }
+
+      it "sets the expected attributes" do
+        expect(number).to have_attributes(
+          degree: 2, size: 14, to_i: 9, to_s: "9"
+        )
+      end
+    end
+
+    describe ":eleven" do
+      subject(:number) { described_class.eleven }
+
+      it "sets the expected attributes" do
+        expect(number).to have_attributes(
+          degree: 4, size: 17, to_i: 11, to_s: "11"
+        )
+      end
+    end
+
+    describe ":thirteen" do
+      subject(:number) { described_class.thirteen }
+
+      it "sets the expected attributes" do
+        expect(number).to have_attributes(
+          degree: 6, size: 21, to_i: 13, to_s: "13"
+        )
+      end
+    end
+  end
 end
