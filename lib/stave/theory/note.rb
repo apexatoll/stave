c734321a3543947ca681eae0e99ec80d569ceeaa
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-08-30 13:36:30 +0100

    Create internal Note::PitchClass lookup

--- lib/stave/theory/note.rb
+++ lib/stave/theory/note.rb
@@ -1,6 +1,15 @@
 module Stave
   module Theory
     class Note < Core::Lookup
+      class PitchClass < Core::Lookup
+        variant :c, to_s: "C", to_i: 0
+        variant :d, to_s: "D", to_i: 2
+        variant :e, to_s: "E", to_i: 4
+        variant :f, to_s: "F", to_i: 5
+        variant :g, to_s: "G", to_i: 7
+        variant :a, to_s: "A", to_i: 9
+        variant :b, to_s: "B", to_i: 11
+      end
     end
   end
 end
--- spec/stave/theory/note_spec.rb
+++ spec/stave/theory/note_spec.rb
@@ -1,3 +1,59 @@
 RSpec.describe Stave::Theory::Note do
-  pending :implementation
+  describe Stave::Theory::Note::PitchClass do
+    describe ":c" do
+      subject(:pitch_class) { described_class.c }
+
+      it "sets the expected attributes" do
+        expect(pitch_class).to have_attributes(to_s: "C", to_i: 0)
+      end
+    end
+
+    describe ":d" do
+      subject(:pitch_class) { described_class.d }
+
+      it "sets the expected attributes" do
+        expect(pitch_class).to have_attributes(to_s: "D", to_i: 2)
+      end
+    end
+
+    describe ":e" do
+      subject(:pitch_class) { described_class.e }
+
+      it "sets the expected attributes" do
+        expect(pitch_class).to have_attributes(to_s: "E", to_i: 4)
+      end
+    end
+
+    describe ":f" do
+      subject(:pitch_class) { described_class.f }
+
+      it "sets the expected attributes" do
+        expect(pitch_class).to have_attributes(to_s: "F", to_i: 5)
+      end
+    end
+
+    describe ":g" do
+      subject(:pitch_class) { described_class.g }
+
+      it "sets the expected attributes" do
+        expect(pitch_class).to have_attributes(to_s: "G", to_i: 7)
+      end
+    end
+
+    describe ":a" do
+      subject(:pitch_class) { described_class.a }
+
+      it "sets the expected attributes" do
+        expect(pitch_class).to have_attributes(to_s: "A", to_i: 9)
+      end
+    end
+
+    describe ":b" do
+      subject(:pitch_class) { described_class.b }
+
+      it "sets the expected attributes" do
+        expect(pitch_class).to have_attributes(to_s: "B", to_i: 11)
+      end
+    end
+  end
 end
