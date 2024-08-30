9b9b0e5fa1535b3f8e4987a1c6386a39b720952c
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-08-30 14:01:30 +0100

    Implement Note variants

--- lib/stave/theory/note.rb
+++ lib/stave/theory/note.rb
@@ -10,6 +10,70 @@ module Stave
         variant :a, to_s: "A", to_i: 9
         variant :b, to_s: "B", to_i: 11
       end
+
+      with_options pitch_class: PitchClass.a do
+        variant :a_double_flat,  accidental: Accidental.double_flat
+        variant :a_flat,         accidental: Accidental.flat
+        variant :a_natural,      accidental: Accidental.natural
+        variant :a_sharp,        accidental: Accidental.sharp
+        variant :a_double_sharp, accidental: Accidental.double_sharp
+      end
+
+      with_options pitch_class: PitchClass.b do
+        variant :b_double_flat,  accidental: Accidental.double_flat
+        variant :b_flat,         accidental: Accidental.flat
+        variant :b_natural,      accidental: Accidental.natural
+        variant :b_sharp,        accidental: Accidental.sharp
+        variant :b_double_sharp, accidental: Accidental.double_sharp
+      end
+
+      with_options pitch_class: PitchClass.c do
+        variant :c_double_flat,  accidental: Accidental.double_flat
+        variant :c_flat,         accidental: Accidental.flat
+        variant :c_natural,      accidental: Accidental.natural
+        variant :c_sharp,        accidental: Accidental.sharp
+        variant :c_double_sharp, accidental: Accidental.double_sharp
+      end
+
+      with_options pitch_class: PitchClass.d do
+        variant :d_double_flat,  accidental: Accidental.double_flat
+        variant :d_flat,         accidental: Accidental.flat
+        variant :d_natural,      accidental: Accidental.natural
+        variant :d_sharp,        accidental: Accidental.sharp
+        variant :d_double_sharp, accidental: Accidental.double_sharp
+      end
+
+      with_options pitch_class: PitchClass.e do
+        variant :e_double_flat,  accidental: Accidental.double_flat
+        variant :e_flat,         accidental: Accidental.flat
+        variant :e_natural,      accidental: Accidental.natural
+        variant :e_sharp,        accidental: Accidental.sharp
+        variant :e_double_sharp, accidental: Accidental.double_sharp
+      end
+
+      with_options pitch_class: PitchClass.f do
+        variant :f_double_flat,  accidental: Accidental.double_flat
+        variant :f_flat,         accidental: Accidental.flat
+        variant :f_natural,      accidental: Accidental.natural
+        variant :f_sharp,        accidental: Accidental.sharp
+        variant :f_double_sharp, accidental: Accidental.double_sharp
+      end
+
+      with_options pitch_class: PitchClass.g do
+        variant :g_double_flat,  accidental: Accidental.double_flat
+        variant :g_flat,         accidental: Accidental.flat
+        variant :g_natural,      accidental: Accidental.natural
+        variant :g_sharp,        accidental: Accidental.sharp
+        variant :g_double_sharp, accidental: Accidental.double_sharp
+      end
+
+      def to_s
+        "#{pitch_class}#{accidental}"
+      end
+
+      def to_i
+        (pitch_class.to_i + accidental.transform) % 12
+      end
     end
   end
 end
--- spec/stave/theory/note_spec.rb
+++ spec/stave/theory/note_spec.rb
@@ -56,4 +56,459 @@ RSpec.describe Stave::Theory::Note do
       end
     end
   end
+
+  describe ":c_double_flat" do
+    subject(:note) { described_class.c_double_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.c,
+        accidental: Stave::Theory::Accidental.double_flat,
+        to_s: "C♭♭",
+        to_i: 10
+      )
+    end
+  end
+
+  describe ":c_flat" do
+    subject(:note) { described_class.c_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.c,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "C♭",
+        to_i: 11
+      )
+    end
+  end
+
+  describe ":c_natural" do
+    subject(:note) { described_class.c_natural }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.c,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "C",
+        to_i: 0
+      )
+    end
+  end
+
+  describe ":c_sharp" do
+    subject(:note) { described_class.c_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.c,
+        accidental: Stave::Theory::Accidental.sharp,
+        to_s: "C♯",
+        to_i: 1
+      )
+    end
+  end
+
+  describe ":c_double_sharp" do
+    subject(:note) { described_class.c_double_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.c,
+        accidental: Stave::Theory::Accidental.double_sharp,
+        to_s: "C♯♯",
+        to_i: 2
+      )
+    end
+  end
+
+  describe ":d_double_flat" do
+    subject(:note) { described_class.d_double_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.d,
+        accidental: Stave::Theory::Accidental.double_flat,
+        to_s: "D♭♭",
+        to_i: 0
+      )
+    end
+  end
+
+  describe ":d_flat" do
+    subject(:note) { described_class.d_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.d,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "D♭",
+        to_i: 1
+      )
+    end
+  end
+
+  describe ":d_natural" do
+    subject(:note) { described_class.d_natural }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.d,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "D",
+        to_i: 2
+      )
+    end
+  end
+
+  describe ":d_sharp" do
+    subject(:note) { described_class.d_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.d,
+        accidental: Stave::Theory::Accidental.sharp,
+        to_s: "D♯",
+        to_i: 3
+      )
+    end
+  end
+
+  describe ":d_double_sharp" do
+    subject(:note) { described_class.d_double_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.d,
+        accidental: Stave::Theory::Accidental.double_sharp,
+        to_s: "D♯♯",
+        to_i: 4
+      )
+    end
+  end
+
+  describe ":e_double_flat" do
+    subject(:note) { described_class.e_double_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.e,
+        accidental: Stave::Theory::Accidental.double_flat,
+        to_s: "E♭♭",
+        to_i: 2
+      )
+    end
+  end
+
+  describe ":e_flat" do
+    subject(:note) { described_class.e_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.e,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "E♭",
+        to_i: 3
+      )
+    end
+  end
+
+  describe ":e_natural" do
+    subject(:note) { described_class.e_natural }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.e,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "E",
+        to_i: 4
+      )
+    end
+  end
+
+  describe ":e_sharp" do
+    subject(:note) { described_class.e_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.e,
+        accidental: Stave::Theory::Accidental.sharp,
+        to_s: "E♯",
+        to_i: 5
+      )
+    end
+  end
+
+  describe ":e_double_sharp" do
+    subject(:note) { described_class.e_double_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.e,
+        accidental: Stave::Theory::Accidental.double_sharp,
+        to_s: "E♯♯",
+        to_i: 6
+      )
+    end
+  end
+
+  describe ":f_double_flat" do
+    subject(:note) { described_class.f_double_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.f,
+        accidental: Stave::Theory::Accidental.double_flat,
+        to_s: "F♭♭",
+        to_i: 3
+      )
+    end
+  end
+
+  describe ":f_flat" do
+    subject(:note) { described_class.f_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.f,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "F♭",
+        to_i: 4
+      )
+    end
+  end
+
+  describe ":f_natural" do
+    subject(:note) { described_class.f_natural }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.f,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "F",
+        to_i: 5
+      )
+    end
+  end
+
+  describe ":f_sharp" do
+    subject(:note) { described_class.f_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.f,
+        accidental: Stave::Theory::Accidental.sharp,
+        to_s: "F♯",
+        to_i: 6
+      )
+    end
+  end
+
+  describe ":f_double_sharp" do
+    subject(:note) { described_class.f_double_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.f,
+        accidental: Stave::Theory::Accidental.double_sharp,
+        to_s: "F♯♯",
+        to_i: 7
+      )
+    end
+  end
+
+  describe ":g_double_flat" do
+    subject(:note) { described_class.g_double_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.g,
+        accidental: Stave::Theory::Accidental.double_flat,
+        to_s: "G♭♭",
+        to_i: 5
+      )
+    end
+  end
+
+  describe ":g_flat" do
+    subject(:note) { described_class.g_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.g,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "G♭",
+        to_i: 6
+      )
+    end
+  end
+
+  describe ":g_natural" do
+    subject(:note) { described_class.g_natural }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.g,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "G",
+        to_i: 7
+      )
+    end
+  end
+
+  describe ":g_sharp" do
+    subject(:note) { described_class.g_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.g,
+        accidental: Stave::Theory::Accidental.sharp,
+        to_s: "G♯",
+        to_i: 8
+      )
+    end
+  end
+
+  describe ":g_double_sharp" do
+    subject(:note) { described_class.g_double_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.g,
+        accidental: Stave::Theory::Accidental.double_sharp,
+        to_s: "G♯♯",
+        to_i: 9
+      )
+    end
+  end
+
+  describe ":a_double_flat" do
+    subject(:note) { described_class.a_double_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.a,
+        accidental: Stave::Theory::Accidental.double_flat,
+        to_s: "A♭♭",
+        to_i: 7
+      )
+    end
+  end
+
+  describe ":a_flat" do
+    subject(:note) { described_class.a_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.a,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "A♭",
+        to_i: 8
+      )
+    end
+  end
+
+  describe ":a_natural" do
+    subject(:note) { described_class.a_natural }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.a,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "A",
+        to_i: 9
+      )
+    end
+  end
+
+  describe ":a_sharp" do
+    subject(:note) { described_class.a_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.a,
+        accidental: Stave::Theory::Accidental.sharp,
+        to_s: "A♯",
+        to_i: 10
+      )
+    end
+  end
+
+  describe ":a_double_sharp" do
+    subject(:note) { described_class.a_double_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.a,
+        accidental: Stave::Theory::Accidental.double_sharp,
+        to_s: "A♯♯",
+        to_i: 11
+      )
+    end
+  end
+
+  describe ":b_double_flat" do
+    subject(:note) { described_class.b_double_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.b,
+        accidental: Stave::Theory::Accidental.double_flat,
+        to_s: "B♭♭",
+        to_i: 9
+      )
+    end
+  end
+
+  describe ":b_flat" do
+    subject(:note) { described_class.b_flat }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.b,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "B♭",
+        to_i: 10
+      )
+    end
+  end
+
+  describe ":b_natural" do
+    subject(:note) { described_class.b_natural }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.b,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "B",
+        to_i: 11
+      )
+    end
+  end
+
+  describe ":b_sharp" do
+    subject(:note) { described_class.b_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.b,
+        accidental: Stave::Theory::Accidental.sharp,
+        to_s: "B♯",
+        to_i: 0
+      )
+    end
+  end
+
+  describe ":b_double_sharp" do
+    subject(:note) { described_class.b_double_sharp }
+
+    it "sets the expected attributes" do
+      expect(note).to have_attributes(
+        pitch_class: described_class::PitchClass.b,
+        accidental: Stave::Theory::Accidental.double_sharp,
+        to_s: "B♯♯",
+        to_i: 1
+      )
+    end
+  end
 end
