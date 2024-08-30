42a880f7e051efe2dab074c27eb88554a8810517
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-08-30 14:09:21 +0100

    Create Degree lookup class

--- lib/stave/theory/degree.rb
+++ lib/stave/theory/degree.rb
@@ -1,6 +1,91 @@
 module Stave
   module Theory
-    module Degree < Core::Lookup
+    class Degree < Core::Lookup
+      variant :root,
+              interval: Interval.perfect_unison,
+              accidental: Accidental.natural
+
+      variant :flat_two,
+              interval: Interval.minor_second,
+              accidental: Accidental.flat
+
+      variant :two,
+              interval: Interval.major_second,
+              accidental: Accidental.natural
+
+      variant :sharp_two,
+              interval: Interval.augmented_second,
+              accidental: Accidental.sharp
+
+      variant :flat_three,
+              interval: Interval.minor_third,
+              accidental: Accidental.flat
+
+      variant :three,
+              interval: Interval.major_third,
+              accidental: Accidental.natural
+
+      variant :sharp_three,
+              interval: Interval.augmented_third,
+              accidental: Accidental.sharp
+
+      variant :flat_four,
+              interval: Interval.diminished_fourth,
+              accidental: Accidental.flat
+
+      variant :four,
+              interval: Interval.perfect_fourth,
+              accidental: Accidental.natural
+
+      variant :sharp_four,
+              interval: Interval.augmented_fourth,
+              accidental: Accidental.sharp
+
+      variant :flat_five,
+              interval: Interval.diminished_fifth,
+              accidental: Accidental.flat
+
+      variant :five,
+              interval: Interval.perfect_fifth,
+              accidental: Accidental.natural
+
+      variant :sharp_five,
+              interval: Interval.augmented_fifth,
+              accidental: Accidental.sharp
+
+      variant :flat_six,
+              interval: Interval.minor_sixth,
+              accidental: Accidental.flat
+
+      variant :six,
+              interval: Interval.major_sixth,
+              accidental: Accidental.natural
+
+      variant :sharp_six,
+              interval: Interval.augmented_sixth,
+              accidental: Accidental.sharp
+
+      variant :flat_seven,
+              interval: Interval.minor_seventh,
+              accidental: Accidental.flat
+
+      variant :seven,
+              interval: Interval.major_seventh,
+              accidental: Accidental.natural
+
+      variant :octave,
+              interval: Interval.perfect_octave,
+              accidental: Accidental.natural
+
+      def to_s
+        return "R" if (to_i % 12).zero?
+
+        "#{accidental}#{interval.number}"
+      end
+
+      def to_i
+        interval.to_i
+      end
     end
   end
 end
--- spec/stave/theory/degree_spec.rb
+++ spec/stave/theory/degree_spec.rb
@@ -1,3 +1,248 @@
 RSpec.describe Stave::Theory::Degree do
-  pending :implementation
+  describe ":root" do
+    subject(:degree) { described_class.root }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.perfect_unison,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "R",
+        to_i: 0
+      )
+    end
+  end
+
+  describe ":flat_two" do
+    subject(:degree) { described_class.flat_two }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.minor_second,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "♭2",
+        to_i: 1
+      )
+    end
+  end
+
+  describe ":two" do
+    subject(:degree) { described_class.two }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.major_second,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "2",
+        to_i: 2
+      )
+    end
+  end
+
+  describe ":sharp_two" do
+    subject(:degree) { described_class.sharp_two }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.augmented_second,
+        accidental: Stave::Theory::Accidental.sharp,
+        to_s: "♯2",
+        to_i: 3
+      )
+    end
+  end
+
+  describe ":flat_three" do
+    subject(:degree) { described_class.flat_three }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.minor_third,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "♭3",
+        to_i: 3
+      )
+    end
+  end
+
+  describe ":three" do
+    subject(:degree) { described_class.three }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.major_third,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "3",
+        to_i: 4
+      )
+    end
+  end
+
+  describe ":sharp_three" do
+    subject(:degree) { described_class.sharp_three }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.augmented_third,
+        accidental: Stave::Theory::Accidental.sharp,
+        to_s: "♯3",
+        to_i: 5
+      )
+    end
+  end
+
+  describe ":flat_four" do
+    subject(:degree) { described_class.flat_four }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.diminished_fourth,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "♭4",
+        to_i: 4
+      )
+    end
+  end
+
+  describe ":four" do
+    subject(:degree) { described_class.four }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.perfect_fourth,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "4",
+        to_i: 5
+      )
+    end
+  end
+
+  describe ":sharp_four" do
+    subject(:degree) { described_class.sharp_four }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.augmented_fourth,
+        accidental: Stave::Theory::Accidental.sharp,
+        to_s: "♯4",
+        to_i: 6
+      )
+    end
+  end
+
+  describe ":flat_five" do
+    subject(:degree) { described_class.flat_five }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.diminished_fifth,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "♭5",
+        to_i: 6
+      )
+    end
+  end
+
+  describe ":five" do
+    subject(:degree) { described_class.five }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.perfect_fifth,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "5",
+        to_i: 7
+      )
+    end
+  end
+
+  describe ":sharp_five" do
+    subject(:degree) { described_class.sharp_five }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.augmented_fifth,
+        accidental: Stave::Theory::Accidental.sharp,
+        to_s: "♯5",
+        to_i: 8
+      )
+    end
+  end
+
+  describe ":flat_six" do
+    subject(:degree) { described_class.flat_six }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.minor_sixth,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "♭6",
+        to_i: 8
+      )
+    end
+  end
+
+  describe ":six" do
+    subject(:degree) { described_class.six }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.major_sixth,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "6",
+        to_i: 9
+      )
+    end
+  end
+
+  describe ":sharp_six" do
+    subject(:degree) { described_class.sharp_six }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.augmented_sixth,
+        accidental: Stave::Theory::Accidental.sharp,
+        to_s: "♯6",
+        to_i: 10
+      )
+    end
+  end
+
+  describe ":flat_seven" do
+    subject(:degree) { described_class.flat_seven }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.minor_seventh,
+        accidental: Stave::Theory::Accidental.flat,
+        to_s: "♭7",
+        to_i: 10
+      )
+    end
+  end
+
+  describe ":seven" do
+    subject(:degree) { described_class.seven }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.major_seventh,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "7",
+        to_i: 11
+      )
+    end
+  end
+
+  describe ":octave" do
+    subject(:degree) { described_class.octave }
+
+    it "sets the expected attributes" do
+      expect(degree).to have_attributes(
+        interval: Stave::Theory::Interval.perfect_octave,
+        accidental: Stave::Theory::Accidental.natural,
+        to_s: "R",
+        to_i: 12
+      )
+    end
+  end
 end
