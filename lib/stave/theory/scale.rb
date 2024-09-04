2066e83932dbcf229a4798df969b82836007d78a
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-09-04 05:48:04 +0100

    Implement Scale triad harmonisation

--- lib/stave/theory/scale.rb
+++ lib/stave/theory/scale.rb
@@ -1,6 +1,11 @@
 module Stave
   module Theory
     class Scale < Core::NoteCollection
+      def triads
+        type.triad_types.zip(notes).map do |chord_type, note|
+          Chord.new(type: chord_type, root: note)
+        end
+      end
     end
   end
 end
