a06b24976022503742b9272cb4969811a0c1a2a9
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-09-05 00:01:42 +0100

    Create KeySignature lookup

---
+++ lib/stave/theory/key_signature.rb
@@ -0,0 +1,33 @@
+module Stave
+  module Theory
+    class KeySignature < Core::Lookup
+      class Group < Core::Lookup
+        variant :flats, accidental: Accidental.flat, to_s: "flats"
+        variant :natural, accidental: Accidental.natural, to_s: ""
+        variant :sharps, accidental: Accidental.sharp, to_s: "sharps"
+      end
+
+      variant :natural, group: Group.natural, flat_count: 0, sharp_count: 0
+
+      with_options group: Group.flats, sharp_count: 0 do
+        variant :one_flat, flat_count: 1
+        variant :two_flats, flat_count: 2
+        variant :three_flats, flat_count: 3
+        variant :four_flats, flat_count: 4
+        variant :five_flats, flat_count: 5
+        variant :six_flats, flat_count: 6
+        variant :seven_flats, flat_count: 7
+      end
+
+      with_options group: Group.sharps, flat_count: 0 do
+        variant :one_sharp, sharp_count: 1
+        variant :two_sharps, sharp_count: 2
+        variant :three_sharps, sharp_count: 3
+        variant :four_sharps, sharp_count: 4
+        variant :five_sharps, sharp_count: 5
+        variant :six_sharps, sharp_count: 6
+        variant :seven_sharps, sharp_count: 7
+      end
+    end
+  end
+end
---
+++ spec/stave/theory/key_signature_spec.rb
@@ -0,0 +1,185 @@
+RSpec.describe Stave::Theory::KeySignature do
+  describe ":flats" do
+    describe ":one_flat" do
+      subject(:key_signature) { described_class.one_flat }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.flats,
+          flat_count: 1,
+          sharp_count: 0
+        )
+      end
+    end
+
+    describe ":two_flats" do
+      subject(:key_signature) { described_class.two_flats }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.flats,
+          flat_count: 2,
+          sharp_count: 0
+        )
+      end
+    end
+
+    describe ":three_flats" do
+      subject(:key_signature) { described_class.three_flats }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.flats,
+          flat_count: 3,
+          sharp_count: 0
+        )
+      end
+    end
+
+    describe ":four_flats" do
+      subject(:key_signature) { described_class.four_flats }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.flats,
+          flat_count: 4,
+          sharp_count: 0
+        )
+      end
+    end
+
+    describe ":five_flats" do
+      subject(:key_signature) { described_class.five_flats }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.flats,
+          flat_count: 5,
+          sharp_count: 0
+        )
+      end
+    end
+
+    describe ":six_flats" do
+      subject(:key_signature) { described_class.six_flats }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.flats,
+          flat_count: 6,
+          sharp_count: 0
+        )
+      end
+    end
+
+    describe ":seven_flats" do
+      subject(:key_signature) { described_class.seven_flats }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.flats,
+          flat_count: 7,
+          sharp_count: 0
+        )
+      end
+    end
+  end
+
+  describe ":natural" do
+    subject(:key_signature) { described_class.natural }
+
+    it "sets the expected attributes" do
+      expect(key_signature).to have_attributes(
+        group: described_class::Group.natural,
+        sharp_count: 0,
+        flat_count: 0
+      )
+    end
+  end
+
+  describe ":sharps" do
+    describe ":one_sharp" do
+      subject(:key_signature) { described_class.one_sharp }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.sharps,
+          sharp_count: 1,
+          flat_count: 0
+        )
+      end
+    end
+
+    describe ":two_sharps" do
+      subject(:key_signature) { described_class.two_sharps }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.sharps,
+          sharp_count: 2,
+          flat_count: 0
+        )
+      end
+    end
+
+    describe ":three_sharps" do
+      subject(:key_signature) { described_class.three_sharps }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.sharps,
+          sharp_count: 3,
+          flat_count: 0
+        )
+      end
+    end
+
+    describe ":four_sharps" do
+      subject(:key_signature) { described_class.four_sharps }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.sharps,
+          sharp_count: 4,
+          flat_count: 0
+        )
+      end
+    end
+
+    describe ":five_sharps" do
+      subject(:key_signature) { described_class.five_sharps }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.sharps,
+          sharp_count: 5,
+          flat_count: 0
+        )
+      end
+    end
+
+    describe ":six_sharps" do
+      subject(:key_signature) { described_class.six_sharps }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.sharps,
+          sharp_count: 6,
+          flat_count: 0
+        )
+      end
+    end
+
+    describe ":seven_sharps" do
+      subject(:key_signature) { described_class.seven_sharps }
+
+      it "sets the expected attributes" do
+        expect(key_signature).to have_attributes(
+          group: described_class::Group.sharps,
+          sharp_count: 7,
+          flat_count: 0
+        )
+      end
+    end
+  end
+end
