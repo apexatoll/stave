da95ea4deff9365f49bf252a6f266e13613ff0f3
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-09-04 23:22:22 +0100

    Implement Accidental type predicates

--- lib/stave/theory/accidental.rb
+++ lib/stave/theory/accidental.rb
@@ -6,6 +6,18 @@ module Stave
       variant :natural,      transform: 0,  to_s: ""
       variant :sharp,        transform: 1,  to_s: "♯"
       variant :double_sharp, transform: 2,  to_s: "♯♯"
+
+      def flat?
+        transform.negative?
+      end
+
+      def natural?
+        transform.zero?
+      end
+
+      def sharp?
+        transform.positive?
+      end
     end
   end
 end
--- spec/stave/theory/accidental_spec.rb
+++ spec/stave/theory/accidental_spec.rb
@@ -3,7 +3,13 @@ RSpec.describe Stave::Theory::Accidental do
     subject(:accidental) { described_class.double_flat }
 
     it "sets the expected attributes" do
-      expect(accidental).to have_attributes(transform: -2, to_s: "♭♭")
+      expect(accidental).to have_attributes(
+        transform: -2,
+        to_s: "♭♭",
+        flat?: true,
+        natural?: false,
+        sharp?: false
+      )
     end
   end
 
@@ -11,7 +17,13 @@ RSpec.describe Stave::Theory::Accidental do
     subject(:accidental) { described_class.flat }
 
     it "sets the expected attributes" do
-      expect(accidental).to have_attributes(transform: -1, to_s: "♭")
+      expect(accidental).to have_attributes(
+        transform: -1,
+        to_s: "♭",
+        flat?: true,
+        natural?: false,
+        sharp?: false
+      )
     end
   end
 
@@ -19,7 +31,13 @@ RSpec.describe Stave::Theory::Accidental do
     subject(:accidental) { described_class.natural }
 
     it "sets the expected attributes" do
-      expect(accidental).to have_attributes(transform: 0, to_s: "")
+      expect(accidental).to have_attributes(
+        transform: 0,
+        to_s: "",
+        flat?: false,
+        natural?: true,
+        sharp?: false
+      )
     end
   end
 
@@ -27,7 +45,13 @@ RSpec.describe Stave::Theory::Accidental do
     subject(:accidental) { described_class.sharp }
 
     it "sets the expected attributes" do
-      expect(accidental).to have_attributes(transform: 1, to_s: "♯")
+      expect(accidental).to have_attributes(
+        transform: 1,
+        to_s: "♯",
+        flat?: false,
+        natural?: false,
+        sharp?: true
+      )
     end
   end
 
@@ -35,7 +59,13 @@ RSpec.describe Stave::Theory::Accidental do
     subject(:accidental) { described_class.double_sharp }
 
     it "sets the expected attributes" do
-      expect(accidental).to have_attributes(transform: 2, to_s: "♯♯")
+      expect(accidental).to have_attributes(
+        transform: 2,
+        to_s: "♯♯",
+        flat?: false,
+        natural?: false,
+        sharp?: true
+      )
     end
   end
 end
