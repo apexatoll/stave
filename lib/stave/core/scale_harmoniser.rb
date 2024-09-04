035bf93307d76008f22b621c7e68897b6af21f63
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-09-04 05:15:18 +0100

    Implement ScaleHarmoniser#harmonise!

--- lib/stave/core/scale_harmoniser.rb
+++ lib/stave/core/scale_harmoniser.rb
@@ -7,6 +7,28 @@ module Stave
         @scale_type = scale_type
         @chord_set = chord_set
       end
+
+      def harmonise!
+        scale_type.uniq.map do |degree|
+          Theory::ChordType.find_by(intervals: intervals(degree))
+        end
+      end
+
+      private
+
+      def scale_intervals(degree)
+        scale_type.rotate(degree.position)
+                  .each_slice(chord_set.slice_size)
+                  .map(&:first)
+                  .map(&:interval)
+                  .take(chord_set.note_count)
+      end
+
+      def intervals(degree)
+        intervals = scale_intervals(degree)
+
+        intervals.map { |interval| interval - intervals.first }
+      end
     end
   end
 end
--- spec/stave/core/scale_harmoniser_spec.rb
+++ spec/stave/core/scale_harmoniser_spec.rb
@@ -1,3 +1,31 @@
 RSpec.describe Stave::Core::ScaleHarmoniser do
-  pending :implementation
+  subject(:scale_harmoniser) do
+    described_class.new(scale_type:, chord_set:)
+  end
+
+  describe "#harmonise!" do
+    subject(:chord_types) { scale_harmoniser.harmonise! }
+
+    describe ":major_scale" do
+      let(:scale_type) { Stave::Theory::ScaleType.major }
+
+      describe ":triads" do
+        let(:chord_set) { Stave::Theory::ChordType::Set.triad }
+
+        it "returns the expected chord types" do
+          expect(chord_types).to eq(
+            [
+              Stave::Theory::ChordType.major_triad,
+              Stave::Theory::ChordType.minor_triad,
+              Stave::Theory::ChordType.minor_triad,
+              Stave::Theory::ChordType.major_triad,
+              Stave::Theory::ChordType.major_triad,
+              Stave::Theory::ChordType.minor_triad,
+              Stave::Theory::ChordType.diminished_triad
+            ]
+          )
+        end
+      end
+    end
+  end
 end
