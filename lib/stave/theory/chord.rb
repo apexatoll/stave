f7a5302c4b025dd093379c471f1b2a43020086d2
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-09-04 05:34:41 +0100

    Implement Chord#to_s

--- lib/stave/theory/chord.rb
+++ lib/stave/theory/chord.rb
@@ -1,6 +1,9 @@
 module Stave
   module Theory
     class Chord < Core::NoteCollection
+      def to_s
+        "#{root}#{type}"
+      end
     end
   end
 end
--- spec/stave/theory/chord_spec.rb
+++ spec/stave/theory/chord_spec.rb
@@ -26,4 +26,26 @@ RSpec.describe Stave::Theory::Chord do
       end
     end
   end
+
+  describe "#to_s" do
+    subject(:string) { chord.to_s }
+
+    assertions = YAML.load_file("spec/cases/chord_strings.yml")
+
+    assertions.each do |type_variant, cases|
+      describe ":#{type_variant}" do
+        let(:type) { Stave::Theory::ChordType.new(type_variant) }
+
+        cases.each do |root_variant, expected_string|
+          describe root_variant.to_s do
+            let(:root) { Stave::Theory::Note.new(root_variant) }
+
+            it "returns #{expected_string}" do
+              expect(string).to eq(expected_string)
+            end
+          end
+        end
+      end
+    end
+  end
 end
