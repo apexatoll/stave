a71145f6782b41989f94c2875ec8dc3402cd43d9
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-09-04 14:12:06 +0100

    Implement Scale#sevenths

--- lib/stave/theory/scale.rb
+++ lib/stave/theory/scale.rb
@@ -2,7 +2,17 @@ module Stave
   module Theory
     class Scale < Core::NoteCollection
       def triads
-        type.triad_types.zip(notes).map do |chord_type, note|
+        harmonise!(type.triad_types)
+      end
+
+      def sevenths
+        harmonise!(type.seventh_types)
+      end
+
+      private
+
+      def harmonise!(chord_types)
+        chord_types.zip(notes).map do |chord_type, note|
           Chord.new(type: chord_type, root: note)
         end
       end
--- spec/stave/theory/scale_spec.rb
+++ spec/stave/theory/scale_spec.rb
@@ -46,4 +46,24 @@ RSpec.describe Stave::Theory::Scale do
       end
     end
   end
+
+  describe "#sevenths" do
+    subject(:sevenths) { scale.sevenths }
+
+    each_case(:scale_sevenths) do |type_variant, cases|
+      describe ":#{type_variant}" do
+        let(:type) { Stave::Theory::ScaleType.new(type_variant) }
+
+        cases.each do |root_variant, expected|
+          describe root_variant.to_s do
+            let(:root) { Stave::Theory::Note.new(root_variant) }
+
+            it "returns the expected triads" do
+              expect(sevenths.map(&:to_s)).to eq(expected)
+            end
+          end
+        end
+      end
+    end
+  end
 end
