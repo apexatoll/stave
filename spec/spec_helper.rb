ff842730c95e75b43e80199b3e44285508810942
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-09-04 06:13:50 +0100

    Implement each_case spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require "stave"
+require "yaml"
 
 RSpec.configure do |config|
   Kernel.srand(config.seed)
@@ -10,4 +11,10 @@ RSpec.configure do |config|
   config.mock_with(:rspec) { |mocks| mocks.verify_partial_doubles = true }
 
   config.default_formatter = :doc if config.files_to_run.one?
+
+  def each_case(name, &)
+    cases = YAML.load_file("spec/cases/#{name}.yml")
+
+    cases.each(&)
+  end
 end
