fe976d251219f8ee0ef1302d585fd069160209da
Chris Welham <71787007+apexatoll@users.noreply.github.com>
2024-09-20 20:18:53 +0100

    Implement Chord#inversions

--- lib/stave/theory/chord.rb
+++ lib/stave/theory/chord.rb
@@ -4,6 +4,12 @@ module Stave
       def symbol
         "#{root.symbol}#{type.symbol}"
       end
+
+      def inversions
+        notes.drop(1).zip(type.inversion_types).map do |root, type|
+          ChordInversion.new(root:, type:)
+        end
+      end
     end
   end
 end
--- spec/stave/theory/chord_spec.rb
+++ spec/stave/theory/chord_spec.rb
@@ -46,4 +46,24 @@ RSpec.describe Stave::Theory::Chord do
       end
     end
   end
+
+  describe "#inversions" do
+    subject(:inversions) { chord.inversions }
+
+    each_case(:chord_inversions) do |type_variant, cases|
+      describe ":#{type_variant}" do
+        let(:type) { Stave::Theory::ChordType.new(type_variant) }
+
+        cases.each do |root_variant, expected_inversions|
+          describe root_variant do
+            let(:root) { Stave::Theory::Note.new(root_variant) }
+
+            it "returns #{expected_inversions.inspect}" do
+              expect(inversions.map(&:symbol)).to eq(expected_inversions)
+            end
+          end
+        end
+      end
+    end
+  end
 end
